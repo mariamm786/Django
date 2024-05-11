f90f85a044fd77e531739c6cc02ac8380417fd75
mariam786 <M9ghani@gmail.com>
2024-05-11 21:46:47 +0530

    Base commit

---
+++ Django/devsearch/projects/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
+
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request,'projects/projects.html',context)
+
+
+def project(request,pk):
+    projectObj = Project.objects.get(id=pk)
+    return  render(request,'projects/single-project.html', {'project': projectObj})
+# Create your views here.
+
+
+def createproject(request):
+    form= ProjectForm()
+
+    if request.method == 'POST':
+       form = ProjectForm(request.POST, request.FILES)
+       if form.is_valid():
+           form.save()
+           return redirect('projects')
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html",context)
+
+def updateproject(request,pk):
+    project = Project.objects.get(id=pk)
+    form= ProjectForm(instance=project)
+
+    if request.method == 'POST':
+       form = ProjectForm(request.POST, request.FILES, instance=project)
+       if form.is_valid():
+           form.save()
+           return redirect('projects')
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html",context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'Projects/delete_template.html', context)
+
+
+
+
+    
---
+++ Django/devsearch/users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Profile
+
+# Create your views here.
+
+def profiles(request):
+    profiles = Profile.objects.all()
+    context = {'profiles':profiles}
+    return render(request, 'users/profiles.html', context)
+
+
+def userProfie(request,pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile':profile}
+    return render(request,'users/user-profie.html', context)
